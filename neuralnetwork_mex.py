f04b813e178c049dffaa44a336ec32fac74056b5
casperob <148339805+casperob@users.noreply.github.com>
2025-03-27 13:40:24 +0100

    Update neuralnetwork_mex.py
    change rolling window approach

--- neuralnetwork_mex.py
+++ neuralnetwork_mex.py
@@ -55,8 +55,8 @@ def rolling_backtest_mlp(y, X, window=4, plot=False):
     actual, predicted, dates, residuals = [], [], [], []
 
     for start in range(0, len(y) - window, window):
-        train_y, train_X = y[:start + window], X[:start + window]
-        test_y, test_X = y[start + window:start + 2 * window], X[start + window:start + 2 * window]
+        train = data[: start + window]
+        test = data[start + window: start + window + 1]
 
         if len(test_y) == 0:
             break
